25433fc00f2b1f3bfc74df2c81dcec290c2014db
Diego Pimenta <dnvpimenta@gmail.com>
2025-02-11 19:59:38 +0000

    Versão 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,8 +2,12 @@
 
 int main() {
 
-    int codCid;
-    char* nomeCid;
+    char codCid[100];
+    char nomeCid[50];
+    int popCid;
+    float areaCid;
+    float pibCid;
+    int pontosTur;
    
     printf("\n\n ----- Desafio Super Trunfo -----\n\n");
     printf("\n\n");
@@ -11,10 +15,23 @@ int main() {
     printf("Por favor, siga os passos abaixo.\n\n");
     printf("\n");
     printf("Insira o código da cidade: \n");
-    scanf("%d", &codCid);
+    scanf("%s", codCid);
     printf("Insira o nome da cidade: \n");
     scanf("%s", nomeCid);
-    printf("\nA carta de número %d é da cidade %s. \n", codCid, nomeCid);
-    
+    printf("Qual a população atual da cidade: \n");
+    scanf("%d", &popCid);
+    printf("Qual a área total da cidade em m²: \n");
+    scanf("%f", &areaCid);
+    printf("Qual o PIB atual da cidade: \n");
+    scanf("%f", &pibCid);
+    printf("Quantos Pontos Turísticos tem na cidade: \n");
+    scanf("%d", &pontosTur);
+
+    printf("\n\n ------------------------------------------------------------------------------\n\n");
+    printf("\nA carta de número %s é da cidade %s. \n", codCid, nomeCid);
+    printf("\nA cidade %s possui %d habitantes, em uma área de %f m². \n", nomeCid, popCid, areaCid);
+    printf("\nSeu PIB é de R$ %f, e ela possui %d pontos turísticos. \n", pibCid, pontosTur);
+    printf("\n\n ------------------------------------------------------------------------------\n\n");
+
     return 0;
 }
