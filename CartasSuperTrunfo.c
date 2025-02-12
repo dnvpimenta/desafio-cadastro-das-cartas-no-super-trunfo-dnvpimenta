9aaf2803b5f73f43e8daedec2a32d94d6794bed9
Diego Pimenta <dnvpimenta@gmail.com>
2025-02-12 14:24:14 -0300

    Versão 3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,35 +3,43 @@
 int main() {
 
     char codCid[100];
-    char nomeCid[50];
+    char nomeCid[60];
     int popCid;
     float areaCid;
     float pibCid;
     int pontosTur;
+    float densCid;
+    float pibPer;
    
-    printf("\n\n ----- Desafio Super Trunfo -----\n\n");
+    printf("\n ----- Desafio Super Trunfo -----\n");
     printf("\n\n");
     printf("Etapa de cadastro de cartas!\n\n");
     printf("Por favor, siga os passos abaixo.\n\n");
     printf("\n");
-    printf("Insira o código da cidade: \n");
+    printf("Insira o código da cidade: ");
     scanf("%s", codCid);
-    printf("Insira o nome da cidade: \n");
+    printf("Insira o nome da cidade: ");
     scanf("%s", nomeCid);
-    printf("Qual a população atual da cidade: \n");
+    printf("Qual a população atual da cidade: ");
     scanf("%d", &popCid);
-    printf("Qual a área total da cidade em m²: \n");
+    printf("Qual a área total da cidade em km²: ");
     scanf("%f", &areaCid);
-    printf("Qual o PIB atual da cidade: \n");
+    printf("Qual o PIB atual da cidade: ");
     scanf("%f", &pibCid);
-    printf("Quantos Pontos Turísticos tem na cidade: \n");
+    printf("Quantos Pontos Turísticos tem na cidade: ");
     scanf("%d", &pontosTur);
 
-    printf("\n\n ------------------------------------------------------------------------------\n\n");
-    printf("\nA carta de número %s é da cidade %s. \n", codCid, nomeCid);
-    printf("\nA cidade %s possui %d habitantes, em uma área de %f m². \n", nomeCid, popCid, areaCid);
-    printf("\nSeu PIB é de R$ %f, e ela possui %d pontos turísticos. \n", pibCid, pontosTur);
-    printf("\n\n ------------------------------------------------------------------------------\n\n");
+    densCid = areaCid / popCid;
+    pibPer = pibCid / popCid;
+
+    printf("\n\n------------------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------------------\n\n");
+    printf("\n  A carta de número %s é da cidade %s. \n", codCid, nomeCid);
+    printf("\n  A cidade %s possui %d habitantes, em uma área de %f km². \n", nomeCid, popCid, areaCid);
+    printf("\n  Densidade populacional de %f de pessoas por km². \n", densCid);
+    printf("\n  Seu PIB per capita é de R$ %f, e ela possui %d pontos turísticos. \n", pibPer, pontosTur);
+    printf("\n\n------------------------------------------------------------------------------\n");
+    printf("------------------------------------------------------------------------------\n\n");
 
     return 0;
 }
